acf3385b5eec4edf997b271e1a8985dd6fed5495
pjeonionworld <qkrwndjs9898@gmail.com>
2024-12-26 20:25:34 +0900

    first commit

---
+++ src/test/java/com/cupid/CupidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
